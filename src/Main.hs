147d7e809a925848a67a43b81902b15b4f46c4cc
Christopher Wells <cwellsny@nycap.rr.com>
2016-05-02 09:35:08 -0400

    Get program working
    Get the program working, so that it is able to run tests from specified files.
    
    Also add an example tests file.

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,32 @@ Maintainer  : cwellsny@nycap.rr.com
 -}
 module Main where
 
--- | Runs the program
+import System.Environment (getArgs)
+import System.IO (hClose, hGetContents, openFile, IOMode(ReadMode))
+
+import Parser
+import Test
+
+{-|
+  Gets the files to be tested and runs the tests they contain.
+-}
 main :: IO ()
 main = do
-  putStrLn "Hello World!"
+  args <- getArgs
+  _ <- mapM testFile args
+  return ()
+
+{-|
+  Runs the tests contained in the given file, and prints out their results.
+-}
+testFile :: String -> IO ()
+testFile file = do
+  handle <- openFile file ReadMode
+  contents <- hGetContents handle
+  let sections = toSections contents
+  let definitions = map toDefinition sections
+  let tests = map toTest definitions
+  results <- mapM runTest tests
+  let resultTexts = zipWith showResults tests results
+  _ <- mapM putStrLn resultTexts
+  hClose handle
