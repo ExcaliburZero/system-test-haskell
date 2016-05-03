8b588453c182af322e3c6d2b651ecc53bfeb996f
Christopher Wells <cwellsny@nycap.rr.com>
2016-05-03 13:22:13 -0400

    Improve program end handling
    Improve the handling of when the program ends.
    
    Set it to print out information on how many tests failed, and give a failure status if any tests do not pass or if the file test cannot be parsed properly.
    
    Closes #1
    Closes #2

--- src/Main.hs
+++ src/Main.hs
@@ -14,6 +14,7 @@ import Data.Text
 import qualified Data.ByteString.Lazy as B (readFile)
 import System.Console.ANSI
 import System.Environment (getArgs)
+import System.Exit (exitFailure, exitSuccess)
 import System.IO (hClose, hGetContents, openFile, IOMode(ReadMode))
 
 import Test
@@ -35,11 +36,16 @@ testFile file = do
   let contents = B.readFile file
   parsed <- (eitherDecode <$> contents) :: IO (Either String [Test])
   case parsed of
-    Left err -> putStrLn err
+    Left err -> do
+      putStrLn ("Unable to parse test definition file `" ++ file ++  "`:\n")
+      putStrLn err
+      exitFailure
     Right tests -> do
       results <- mapM runTest tests
       zipWithM_ printResults tests results
-      return ()
+      let total = Prelude.length results
+      let failures = Prelude.length $ Prelude.filter (\ (x, _) -> not x) results
+      endProgram failures total
 
 {-|
   Prints out the results of the given test with a color based on the results.
@@ -51,3 +57,17 @@ printResults test results = do
     (True, _) -> setSGR [SetColor Foreground Dull Green]
     (False, _) -> setSGR [SetColor Foreground Dull Red]
   putStrLn $ showResults test results
+  setSGR [Reset]
+
+{-|
+  Handles the ending of the program. Prints out the number of tests that failed,
+  if any. Also gives an exit failure if any tests failed.
+-}
+endProgram :: Int -> Int -> IO ()
+endProgram failures total = case failures of
+  0 -> do
+    putStrLn "\nAll tests passed"
+    exitSuccess
+  _ -> do
+    putStrLn ("\n" ++ show failures ++ " / " ++ show total ++ " tests failed")
+    exitFailure
