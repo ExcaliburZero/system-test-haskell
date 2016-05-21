8b8d8894e1f316b8e3701763eacb5bd92cb7acb9
Christopher Wells <cwellsny@nycap.rr.com>
2016-05-21 19:39:40 -0400

    Add test for showResults with a passing test
    Add a unit test for the showResults function with the case where a passing test is given.

--- tests/unit/Main.hs
+++ tests/unit/Main.hs
@@ -14,11 +14,22 @@ import SystemTest.Test
 -- | Runs the unit tests of the program
 main :: IO Counts
 main = do
-  let tests = TestList [TestLabel "showResults" testShowResults]
+  let tests = TestList [ TestLabel "showResults Pass" testShowResultsPass
+                       , TestLabel "showResults Fail" testShowResultsFail
+		       ]
   runTestTT tests
 
-testShowResults :: Test.HUnit.Test
-testShowResults = TestCase (assertEqual "test" expected actual)
+-- | A case of the showResults function where a passing test is given.
+testShowResultsPass :: Test.HUnit.Test
+testShowResultsPass = TestCase (assertEqual "showResults Pass" expected actual)
+  where expected = "HelloTest: Passed"
+        actual   = showResults testDef result
+        testDef  = Test "HelloTest" "echo 'Hello, World!'" "Hello, World!"
+        result   = (True, "Hello, World!")
+
+-- | A case of the showResults function where a failing test is given.
+testShowResultsFail :: Test.HUnit.Test
+testShowResultsFail = TestCase (assertEqual "showResults Fail" expected actual)
   where expected = "GoodbyeTest: Failed\n  Expected: Hello, World!\n  Actual:   Goodbye, World!"
         actual   = showResults testDef result
         testDef  = Test "GoodbyeTest" "echo 'Goodbye, World!'" "Hello, World!"
